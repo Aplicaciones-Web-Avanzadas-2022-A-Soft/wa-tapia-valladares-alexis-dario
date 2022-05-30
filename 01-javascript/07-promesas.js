34643d4304af503747e92fd55a4c59f6e3a3429e
AlexisTapia <tapiaalexvpp@gamil.com>
2022-05-30 07:38:17 -0500

    Ejercicio 7
    Ejercicio 7 parecido al ejercicio del deber - corregir deber

--- 01-javascript/07-promesas.js
+++ 01-javascript/07-promesas.js
@@ -1,34 +1,45 @@
 const fs = require('fs');
 
-function promesaEsPar(numero){
+function promesaEsPar(numero) {
     const miPrimeraPromesa = new Promise(
         ( //Definicon de la promesa
-        resolve, //funcinon return
-        reject   //funcion throw
-    ) => {
-        if(numero%2 === 0){
-            resolve(numero);
-        }else{
-            reject('No es par =(');
+            resolve, //funcinon return
+            reject   //funcion throw
+        ) => {
+            if (numero % 2 === 0) {
+                resolve(numero);
+            } else {
+                reject('No es par =(');
+            }
         }
-    }
     )
     return miPrimeraPromesa
 }
 
 promesaEsPar(3)
     .then(//return
-        (respuesta)=>{
+        (respuesta) => {
             console.log('Respuesta : ', respuesta);
         }
     )
     .catch(//throw
-        (error)=>{
-            console.log('Error : ',error);
+        (error) => {
+            console.log('Error : ', error);
         }
     )
     .finally(//finally
-        ()=>{
+        () => {
             console.info('FIN');
         }
     )
+
+function promesasElevarAlCuadrado(numero){
+    const miPrimeraPromesa = new Promise(
+        (resolve, reject) => {
+            const numeroElevadoAlCuadrado = Math.pow(numero,2);
+            resolve(numeroElevadoAlCuadrado);
+        }
+    )
+    return miPrimeraPromesa;
+}
+
